12c62d600fcf8a2dcb8330e0d7e01bcd26beebe9
Alexander Pavlyuk <evalexhimself@gmail.com>
2021-06-04 10:43:19 +0300

    feat: mock tracks list (#20)

--- frontend/libs/geo-search/feature-mock-gsm-tracks/src/lib/tracks-list/tracks-list.component.ts
+++ frontend/libs/geo-search/feature-mock-gsm-tracks/src/lib/tracks-list/tracks-list.component.ts
@@ -6,6 +6,7 @@ import {
   EventEmitter,
   Output,
 } from '@angular/core';
+import { MatSelectionListChange } from '@angular/material/list';
 import { GeoPoint } from '@gsm-geo-delimitation/shared/util-geolocation';
 
 @Component({
@@ -20,4 +21,17 @@ export class TracksListComponent {
 
   @Output()
   readonly tracksChange = new EventEmitter<GeoPoint[][]>();
+
+  @Input()
+  selectedTrackIndex: number;
+
+  @Output()
+  readonly selectedTrackIndexChange = new EventEmitter<number>();
+
+  onSelectionChange({
+    options: [{ value: selectedTrackIndex }],
+  }: MatSelectionListChange) {
+    this.selectedTrackIndex = selectedTrackIndex;
+    this.selectedTrackIndexChange.emit(selectedTrackIndex);
+  }
 }
